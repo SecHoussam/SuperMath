5d7b6976730da595937142a79f8f6351dceb8e67
Houssam Miliani <149241633+SecHoussam@users.noreply.github.com>
2025-12-23 17:02:33 +0100

    Update SuperMath.h

--- src/SuperMath.h
+++ src/SuperMath.h
@@ -1,1 +1,24 @@
+#include <iostream>
+namespace {
 
+    int MaxOf2Number(int a, int b){
+
+      if (a > b){
+
+        return a;
+      }else {
+        return b;
+      }
+      
+    }
+
+int MinOf2Number(int a, int b){
+
+      if (a < b){
+
+        return a;
+      }else {
+        return b;
+      } 
+    }
+}
