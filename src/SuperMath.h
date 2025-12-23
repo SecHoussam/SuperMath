6944917daca1820dcd2c8b01344106b638f17655
Houssam Miliani <149241633+SecHoussam@users.noreply.github.com>
2025-12-23 17:49:21 +0100

    Refactor MaxOf2Number and MinOf2Number; add MaxOf3

--- src/SuperMath.h
+++ src/SuperMath.h
@@ -2,7 +2,7 @@
 #include <iostream>
 namespace SuperMath{
 
-int MaxOf2Number(int a, int b){
+int MaxOf2(int a, int b){
       if (a > b){
             return a;
       }else {
@@ -10,7 +10,7 @@ int MaxOf2Number(int a, int b){
       }
 }
 
-int MinOf2Number(int a, int b){
+int MinOf2(int a, int b){
       if (a < b){
             return a;
       }else {
@@ -18,10 +18,26 @@ int MinOf2Number(int a, int b){
       }
 }
 
+int MaxOf3(int a, int b, int c){
+
+      if (a > b){
+            if ( a > c){
+                  return a;
+            }else {
+                  return c;
+            }
+            
+      }else {
+            if (b > c){
+                  return b;
+            }else {
+                  return c;
+            }
+            
+      }
 
 
-
-
+}
 
 
 
